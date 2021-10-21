895dc36f7d030b04651b10b682be3060a693b60b
kevduc <64347790+kevduc@users.noreply.github.com>
2021-10-21 12:17:08 +0100

    Cube movement as parameters + raised cubes

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,12 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour { 
+public class Rotator : MonoBehaviour
+{
+    public Vector3 rotationSpeed = new Vector3(0, 0, 0);
+
+    public float floatingSpeed = 0;
+
+    public float floatingAmplitude = 0;
+
+    private float initialY;
+
+    private void Start()
+    {
+        initialY = transform.position.y;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(20, 30, 25) * Time.deltaTime);
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+        Vector3 position = transform.position;
+        position.y =
+            initialY +
+            (float) Math.Sin(Time.timeAsDouble * floatingSpeed) *
+            floatingAmplitude;
+        transform.position = position;
     }
 }
